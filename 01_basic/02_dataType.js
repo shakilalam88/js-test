51a8733a7e4055095ea7dd22ff1c49c85dfa5d83
Shakil Alam <50899183+shakilalam88@users.noreply.github.com>
2023-08-10 07:32:45 +0000

    comparision js

--- 01_basic/02_dataType.js
+++ 01_basic/02_dataType.js
@@ -1,3 +1,9 @@
+// JavaSrcipt is dynamic type language because the interpreter assigns variables a type at runtime based on the variable's value at the time.
+
+
+
+
+
 "use strict"; //treat all JS code as newer verstion
 
 // alert("hello") // we are using nodejs, not browser
@@ -8,17 +14,35 @@ let isLoggedIn = false;
 
 // data_type
 
- //prmitive value
+   //prmitive value
     
-    //TYPE            //TYPEOF
+      //TYPE            //TYPEOF
 
-    //number          //"number"
-    //null            //"object"
-    //bignit          //"bigint"
-    //boolean         //"boolean"
-    //string          //"string"
-    //symbol          //"symbol"
-    //undefined       //"undefined"
+      //number          //"number"     let value = 323;
+      //null            //"object"     let value = null (or return null value)
+      //bignit          //"bigint"     let value = 3243243243223432n
+      //boolean         //"boolean"    let value = true
+      //string          //"string"     let value = "hello"
+      //symbol          //"symbol"     let id = Symbol('123')
+      //undefined       //"undefined"  let value;
+
+   //Non prmitive value (Reference)
+   
+      // Array
+      // Object
+      // function
 
 
-    //object
+
+      // Type and typeof return value
+         // number - number
+         // string -  String
+         // boolean - boolean
+         // symbol - symbol
+         // bigint - bigint
+         // undefined - undefined
+
+         // null - object
+         // array - object
+         // object - object
+         // function - function
---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,34 @@
+// Comparison and Logical operators are used to test for true or false.
+
+//console.log(2 > 1);         // true
+//console.log(2 < 1);         // false
+//console.log(3 >= 3);        // true
+//console.log(3 <=2);         // false
+//console.log(3 != 4);        // true
+//console.log(3 == 3);        // true
+//console.log(3 == "3");      // true
+//console.log(3 === "3");     // false
+//console.log(3 === +"3");      // true
+
+//console.log("2" > 1);         // true
+//console.log("02" > 1 );       // true
+//console.log("02" < 3);        // true
+
+console.log("2" > "12");        // true
+console.log("2" < "123442");    // false
+// When comparing two strings, "2" will be greater than "12", 
+//because (first alphabet) 1 is less than 2.
+
+
+
+//console.log(null > 0);          // false
+//console.log(null == 0);         // false
+//console.log(null >= 0);         // ture  Comparioson operator covert null to number,treating it as 0
+
+//console.log(undefined > 0);          // false
+//console.log(undefined == 0);         // false
+//console.log(undefined >= 0);         // false
+//console.log(undefined <= 0);        // false
+//console.log(undefined != 0);        // ture
+
+
