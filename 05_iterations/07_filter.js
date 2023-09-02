33b02b1d8388fe182abd7989f166d8013725b649
shakil_alam <shakilalam88@gmail.com>
2023-09-02 10:39:29 +0530

    filter and reduce

--- 05_iterations/07_filter.js
+++ 05_iterations/07_filter.js
@@ -13,7 +13,7 @@ const newNums = myNums.filter((num) => {
   return num > 5
 })
 console.log(newNums) //[ 6, 7, 8, 9, 10 ]
-console.log(myNums)
+console.log(myNums) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
